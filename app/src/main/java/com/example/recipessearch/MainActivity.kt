99262a766e91a35b3ba8fcc47ad40e43be1d3d19
OxotaPiva <stepan-vlasov-02@mail.ru>
2023-08-26 13:33:08 +0700

    display saved recipes on the home screen

--- app/src/main/java/com/example/recipessearch/MainActivity.kt
+++ app/src/main/java/com/example/recipessearch/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipessearch.adapters.RecipesAdapter
+import com.example.recipessearch.data.api.ApiService
+import com.example.recipessearch.data.api.RecipeHit
 import com.example.recipessearch.data.api.RecipeSearchResponse
+import com.example.recipessearch.data.db.RecipesDao
+import com.example.recipessearch.data.db.RecipesDatabase
 import com.example.recipessearch.databinding.ActivityMainBinding
 import com.example.skytracker.data.api.Instance
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         lateinit var binding: ActivityMainBinding
     }
-
+    private lateinit var recipesDao: RecipesDao
     private lateinit var recipesAdapter: RecipesAdapter
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val service = Instance.api
+        getSavedListRecipes(service)
         binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 GlobalScope.launch {
@@ -39,10 +43,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-//                if (newText.isEmpty()) {
-//                    val call = service.getVacancies()
-//                    fetchVacancies(call)
-//                }
+                if (newText.isEmpty()) {
+                    getSavedListRecipes(service)
+                }
                 return true
             }
 
@@ -68,5 +71,45 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, t.toString(), Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    private fun getSavedListRecipes(service: ApiService) {
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                recipesDao = RecipesDatabase
+                    .getDatabase(this@MainActivity)
+                    .recipesDao()
+                val recipesDB = recipesDao.getAllRecipes()
+                val recipesList = emptyList<RecipeHit>()
+                val recipes = recipesList.toMutableList()
+                recipesDB.forEach { recipe ->
+                    val recipeCall = service.getRecipeByUri(recipe.uri)
+                    recipeCall.enqueue(object : Callback<RecipeSearchResponse> {
+                        override fun onResponse(call: Call<RecipeSearchResponse>, response: Response<RecipeSearchResponse>) {
+                            if (response.isSuccessful) {
+                                val response = response.body()
+                                val hits = response?.hits
+                                hits?.get(0)?.let { recipes.add(it) }
+                            } else {
+                                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<RecipeSearchResponse>, t: Throwable) {
+                            Toast.makeText(applicationContext, t.toString(), Toast.LENGTH_LONG).show()
+                        }
+                    })
+                }
+                runOnUiThread {
+                    recipesAdapter =
+                        RecipesAdapter(recipes, this@MainActivity)
+                    binding.recipeAdapter.adapter = recipesAdapter
+                    binding.recipeAdapter.layoutManager =
+                        LinearLayoutManager(this@MainActivity)
+                }
+            }
+
+        }
+
     }
 }
